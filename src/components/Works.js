2ce17ea9c4ae4e3f62947a86a8ef2f7a99ff1feb
Markus Wedler <markuswedler8@gmail.com>
2023-01-11 00:32:42 +1000

    .

--- src/components/Works.js
+++ src/components/Works.js
@@ -14,7 +14,7 @@ const Works = ({ works }) => {
       <h2 className="title">{ works && works.title }</h2>
       <div className="works">{
         works && Object.keys(works.cards).map(name => (
-          <a href={ "https://" + works.cards[name].url } key={name}>
+          <a href={ "https://" + works.cards[name].url } target="_blank" key={name}>
             <div className="img"  style={{ backgroundImage: "url('/images/works_logos/" + name + ".svg')" }}/>
             <h3>{ works.cards[name].title }</h3>
             <p>{ works.cards[name].paragraph }</p>
